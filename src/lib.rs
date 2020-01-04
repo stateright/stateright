efff53b1fac54a64076a43babd6caa23ff151e3d
Jonathan Nadal <jon.nadal@gmail.com>
2020-01-04 11:35:05 -0800

    factor out `test_util::linear_equation_solver`
    A previous commit similarly introduced `test_util::binary_clock`.
    The goal is to have some minimal state machines for unit tests.

--- src/lib.rs
+++ src/lib.rs
@@ -527,42 +527,8 @@ pub fn hash<T: Hash>(value: &T) -> Fingerprint {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::test_util::linear_equation_solver::*;
     use std::num::Wrapping;
-
-    /// Given `a`, `b`, and `c`, finds `x` and `y` such that `a*x + b*y = c` where all values are
-    /// in `Wrapping<u8>`.
-    struct LinearEquation { a: u8, b: u8, c: u8 }
-    #[derive(Clone, Debug, Eq, PartialEq)]
-    enum Guess { IncreaseX, IncreaseY }
-    impl StateMachine for LinearEquation {
-        type State = (Wrapping<u8>, Wrapping<u8>);
-        type Action = Guess;
-
-        fn init_states(&self) -> Vec<Self::State> {
-            vec![(Wrapping(0), Wrapping(0))]
-        }
-
-        fn actions(&self, _state: &Self::State, actions: &mut Vec<Self::Action>) {
-            actions.push(Guess::IncreaseX);
-            actions.push(Guess::IncreaseY);
-        }
-
-        fn next_state(&self, state: &Self::State, action: &Self::Action) -> Option<Self::State> {
-            let (x, y) = *state;
-            match &action {
-                Guess::IncreaseX => Some((x + Wrapping(1), y)),
-                Guess::IncreaseY => Some((x, y + Wrapping(1))),
-            }
-        }
-    }
-    fn invariant(equation: &LinearEquation, solution: &(Wrapping<u8>, Wrapping<u8>)) -> bool {
-        match *solution {
-            (x, y) => {
-                Wrapping(equation.a)*x + Wrapping(equation.b)*y != Wrapping(equation.c)
-            }
-        }
-    }
-
 
     #[test]
     fn model_check_records_states() {
