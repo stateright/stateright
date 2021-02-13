1d2245dc0f798c30d5695494f93b68e9bf6c93b2
Jonathan Nadal <jon.nadal@gmail.com>
2021-02-13 05:17:26 -0800

    add optional filter to bench.sh
    Example: `./bench.sh 2 register`
    
    This commit also adds a message indicating the inferred script inputs.

--- bench.sh
+++ bench.sh
@@ -4,12 +4,24 @@ set -e
 set -u
 
 COUNT=${1:-3}
+FILTER=${2:-''}
 
+echo "Benchmarking with:" >&2
+echo "- COUNT=$COUNT"     >&2
+echo "- FILTER='$FILTER'" >&2
+echo                      >&2
+
+# USAGE: bench EXAMPLE ARGS...
+# EXAMPLE: bench 2pc check 9
+# 
+# No-op if EXAMPLE does not match FILTER.
 bench() {
-    echo "== $@ =="
-    for i in $(seq $COUNT); do
-        cargo -q run --release --example "$@" |grep "sec="
-    done
+    if [[ $1 == *"$FILTER"* ]]; then
+        echo "== $@ =="
+        for i in $(seq $COUNT); do
+            cargo -q run --release --example "$@" |grep "sec="
+        done
+    fi
 }
 
 bench 2pc check 9
