e501c949e62d99547ad3f25a22b690f5d3657c73
Jonathan Nadal <jon.nadal@gmail.com>
2021-12-12 17:03:41 -0800

    add docs to the `increment` example
    These are some notes I wrote myself when reviewing the example. They may
    be helpful to others who want to contribute to this work in progress
    branch.

--- examples/increment.rs
+++ examples/increment.rs
@@ -1,16 +1,127 @@
+//! # This Example
+//!
+//! This example illustrates a multithreaded system analogous to the following pseudocode (where
+//! numeric labels inside the `spawn` command indicate atomic instructions):
+//!
+//! ```
+//! SHARED = 0;
+//! for _ in 0..N {
+//!     spawn({
+//!            let thread_local = 0;
+//!         1: thread_local = SHARED;
+//!         2: SHARED = thread_local + 1;
+//!         3:
+//!     })
+//! }
+//! ```
+//!
+//! # A Possible Intended Invariant
+//!
+//! If each thread was run in sequence, we would maintain an invariant whereby `SHARED`
+//! always equals the number of threads that have finished executing atomic instruction #2. Since
+//! the threads interleave, the final value can be smaller. For example, with two threads:
+//!
+//! ```
+//! - Read(1)  // thread #1 reads 0 from shared memory into local memory
+//! - Read(0)  // thread #0 reads 0 from shared memory into local memory
+//! - Write(1) // thread #1 finishes by writing 1 into shared memory, maintaining the invariant
+//! - Write(0) // thread #0 finishes by rewriting 1 into shared memory, breaking the invariant
+//! ```
+//!
+//! # State Space Without Symmetry Reduction
+//!
+//! In the rest of this explanation, to match the implementation we use `i` to represent `SHARED`,
+//! `ts[_]` to represent each `thread_local`, and `pcs[_]` to represent each program counter.
+//!
+//! Without symmetry reduction, the state space is comprised of 13 unique states:
+//!
+//! ```
+//! // 1. The system has a deterministic initial state.
+//! State { i: 0, ts: [0, 0], pcs: [1, 1] }
+//!
+//! // 2. Next one of the threads reads from shared memory, resulting in two possible states.
+//! State { i: 0, ts: [0, 0], pcs: [2, 1] }
+//! State { i: 0, ts: [0, 0], pcs: [1, 2] }
+//!
+//! // 3a. Then either the same thread writes to shared memory...
+//! State { i: 1, ts: [0, 0], pcs: [3, 1] }
+//! State { i: 1, ts: [0, 0], pcs: [1, 3] }
+//!
+//! // 3b. ... or the other thread catches up by also reading from shared memory.
+//! State { i: 0, ts: [0, 0], pcs: [2, 2] }
+//!
+//! // 4a. In the case where the same thread wrote to shared memory, the other thread must observe
+//! //     that write and will then must persist the increment, maintaining the invariant.
+//! //
+//! //     The read:
+//! State { i: 1, ts: [1, 0], pcs: [2, 3] }
+//! State { i: 1, ts: [0, 1], pcs: [3, 2] }
+//! //     The write:
+//! State { i: 2, ts: [1, 0], pcs: [3, 3] }
+//! State { i: 2, ts: [0, 1], pcs: [3, 3] }
+//!
+//! // 4b. Otherwise (in the case where both threads read the original shared memory), one writes
+//! //     the increment (maintaining the invariant) before the other writes the stale increment
+//! //     (breaking the invariant).
+//! //
+//! //     First write:
+//! State { i: 1, ts: [0, 0], pcs: [2, 3] }
+//! State { i: 1, ts: [0, 0], pcs: [3, 2] }
+//! //     Second write:
+//! State { i: 1, ts: [0, 0], pcs: [3, 3] }
+//! ```
+//!
+//! # State Space With Symmetry Reduction
+//!
+//! All threads are identical, so symmetry reduction can be employed to reduce the state space from
+//! 13 to 8, eliminating 5 states:
+//!
+//! ```
+//! // 1. Same as without symmetry reduction.
+//! State { i: 0, ts: [0, 0], pcs: [1, 1] }
+//!
+//! // 2. Reduction eliminates 1 state.
+//! State { i: 0, ts: [0, 0], pcs: [2, 1] }
+//!
+//! // 3a. Reduction eliminates 1 state.
+//! State { i: 1, ts: [0, 0], pcs: [3, 1] }
+//!
+//! // 3b. Same as without symmetry reduction.
+//! State { i: 0, ts: [0, 0], pcs: [2, 2] }
+//!
+//! // 4a. Reduction eliminates 2 states.
+//! //
+//! //     Read:
+//! State { i: 1, ts: [0, 1], pcs: [3, 2] }
+//! //     Write:
+//! State { i: 2, ts: [0, 1], pcs: [3, 3] }
+//!
+//! // 4b. Reduction eliminates 1 state.
+//! //
+//! //     First write:
+//! State { i: 1, ts: [0, 0], pcs: [3, 2] }
+//! //     Second write:
+//! State { i: 1, ts: [0, 0], pcs: [3, 3] }
+//! ```
+
 use stateright::*;
 use itertools::*;
 
 #[derive(Debug, Clone)]
 pub enum Action {
+    /// A specified thread reads from the shared state into its local state.
     Read(usize),
+    /// A specified thread writes its local state to the shared state.
     Write(usize),
 }
 
 #[derive(Debug, Clone, Default, Hash)]
 pub struct State {
+    /// The shared global state.
     i: u8,
+    /// Each thread's internal "thread local" state.
     ts: Vec<u8>,
+    /// Each thread's program counter.
     pcs: Vec<u8>,
 }
 
