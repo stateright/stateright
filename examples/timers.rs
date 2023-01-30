83279a1a25081e94bd91ee1d332db3772a826e81
Andrew Jeffery <dev@jeffas.io>
2023-01-30 19:56:07 +0000

    Update timers example

--- examples/timers.rs
+++ examples/timers.rs
@@ -1,32 +1,33 @@
 use serde::{Deserialize, Serialize};
-use stateright::report::WriteReporter;
 use stateright::actor::model_timeout;
-use stateright::{Model, Checker};
-use stateright::actor::{
-    Actor, ActorModel, Id, model_peers, Network,Out};
+use stateright::actor::{model_peers, Actor, ActorModel, Id, Network, Out};
+use stateright::report::WriteReporter;
+use stateright::{Checker, Model};
 use std::borrow::Cow;
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 enum PingerMsg {
     Ping,
     Pong,
 }
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 enum PingerTimer {
     Even,
     Odd,
+    NoOp,
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct PingerState {
-    received: usize
+    sent: usize,
+    received: usize,
 }
 
 #[derive(Clone)]
-struct PingerActor { peer_ids: Vec<Id> }
+struct PingerActor {
+    peer_ids: Vec<Id>,
+}
 
 impl Actor for PingerActor {
     type Msg = PingerMsg;
@@ -36,13 +37,23 @@ impl Actor for PingerActor {
     fn on_start(&self, _id: Id, o: &mut Out<Self>) -> Self::State {
         o.set_timer(PingerTimer::Even, model_timeout());
         o.set_timer(PingerTimer::Odd, model_timeout());
-        PingerState { received: 0 }
+        o.set_timer(PingerTimer::NoOp, model_timeout());
+        PingerState {
+            sent: 0,
+            received: 0,
+        }
     }
 
-    fn on_msg(&self, _id: Id, state: &mut Cow<Self::State>, src: Id, msg: Self::Msg, o: &mut Out<Self>) {
+    fn on_msg(
+        &self,
+        _id: Id,
+        state: &mut Cow<Self::State>,
+        src: Id,
+        msg: Self::Msg,
+        o: &mut Out<Self>,
+    ) {
         match msg {
             PingerMsg::Ping => {
-                state.to_mut().received += 2;
                 o.send(src, PingerMsg::Pong);
             }
             PingerMsg::Pong => {
@@ -51,12 +62,19 @@ impl Actor for PingerActor {
         }
     }
 
-    fn on_timeout(&self, _id: Id, _state: &mut Cow<Self::State>, timer: &Self::Timer, o: &mut Out<Self>) {
+    fn on_timeout(
+        &self,
+        _id: Id,
+        state: &mut Cow<Self::State>,
+        timer: &Self::Timer,
+        o: &mut Out<Self>,
+    ) {
         match timer {
             PingerTimer::Even => {
                 o.set_timer(PingerTimer::Even, model_timeout());
                 for &dst in &self.peer_ids {
                     if usize::from(dst) % 2 == 0 {
+                        state.to_mut().sent += 1;
                         o.send(dst, PingerMsg::Ping);
                     }
                 }
@@ -65,9 +83,13 @@ impl Actor for PingerActor {
                 o.set_timer(PingerTimer::Odd, model_timeout());
                 for &dst in &self.peer_ids {
                     if usize::from(dst) % 2 != 0 {
+                        state.to_mut().sent += 1;
                         o.send(dst, PingerMsg::Ping);
                     }
                 }
+            }
+            PingerTimer::NoOp => {
+                o.set_timer(PingerTimer::NoOp, model_timeout());
             }
         }
     }
@@ -80,57 +102,54 @@ struct PingerModelCfg {
 }
 
 impl PingerModelCfg {
-    fn into_model(self) ->
-        ActorModel<
-            PingerActor,
-            Self,
-            ()>
-    {
-        ActorModel::new(
-                self.clone(),
-                (),
-            )
-            .actors((0..self.server_count)
-                .map(|i| PingerActor {
-                    peer_ids: model_peers(i, self.server_count),
-                }))
+    fn into_model(self) -> ActorModel<PingerActor, Self, ()> {
+        ActorModel::new(self.clone(), ())
+            .actors((0..self.server_count).map(|i| PingerActor {
+                peer_ids: model_peers(i, self.server_count),
+            }))
             .init_network(self.network)
+            .property(stateright::Expectation::Always, "true", |_, _| true)
     }
 }
 
 fn main() -> Result<(), pico_args::Error> {
-    env_logger::init_from_env(env_logger::Env::default()
-        .default_filter_or("info")); // `RUST_LOG=${LEVEL}` env variable to override
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info")); // `RUST_LOG=${LEVEL}` env variable to override
 
     let mut args = pico_args::Arguments::from_env();
     match args.subcommand()?.as_deref() {
         Some("check") => {
-            let network = args.opt_free_from_str()?
+            let network = args
+                .opt_free_from_str()?
                 .unwrap_or(Network::new_unordered_nonduplicating([]))
                 .into();
             println!("Model checking Pingers");
             PingerModelCfg {
-                    server_count: 3,
-                    network,
-                }
-                .into_model().checker().threads(num_cpus::get())
-                .spawn_dfs().report(&mut WriteReporter::new(&mut std::io::stdout()));
+                server_count: 3,
+                network,
+            }
+            .into_model()
+            .checker()
+            .threads(num_cpus::get())
+            .spawn_dfs()
+            .report(&mut WriteReporter::new(&mut std::io::stdout()));
         }
         Some("explore") => {
-            let address = args.opt_free_from_str()?
+            let address = args
+                .opt_free_from_str()?
                 .unwrap_or("localhost:3000".to_string());
-            let network = args.opt_free_from_str()?
+            let network = args
+                .opt_free_from_str()?
                 .unwrap_or(Network::new_unordered_nonduplicating([]))
                 .into();
-            println!(
-                "Exploring state space for Pingers on {}.",
-                address);
+            println!("Exploring state space for Pingers on {}.", address);
             PingerModelCfg {
-                    server_count: 3,
-                    network,
-                }
-                .into_model().checker().threads(num_cpus::get())
-                .serve(address);
+                server_count: 3,
+                network,
+            }
+            .into_model()
+            .checker()
+            .threads(num_cpus::get())
+            .serve(address);
         }
         // Some("spawn") => {
         //     let port = 3000;
@@ -164,7 +183,10 @@ fn main() -> Result<(), pico_args::Error> {
             // println!("  ./paxos check [CLIENT_COUNT] [NETWORK]");
             println!("  ./timers explore [CLIENT_COUNT] [ADDRESS] [NETWORK]");
             // println!("  ./paxos spawn");
-            println!("NETWORK: {}", Network::<<PingerActor as Actor>::Msg>::names().join(" | "));
+            println!(
+                "NETWORK: {}",
+                Network::<<PingerActor as Actor>::Msg>::names().join(" | ")
+            );
         }
     }
 
